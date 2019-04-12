775d9fa5f246876c3732ff9c39b7d0004161653f
Mak Tsz Fung <tszfmak7-c@my.cityu.edu.hk>
2019-04-13 02:22:00 +0800

    Update LoanControl.h

--- Project/Project/LoanControl.h
+++ Project/Project/LoanControl.h
@@ -4,22 +4,37 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-using namespace std;
 
+#include "Equipment.h"
+#include "User.h"
+#include "FileHandler.h"
 
+void prepare(User*& user, Tent*& tents, Stove*& stoves, Lantern*& lanterns, string**& records, int &size);
 
-class ControlUser
-{
+string getDate();
 
-public:
-	
-};
+bool isWordCharacter(char c);
 
-class ControlEquip 
-{
+void getUserInformation(User*& user, string userInput);
 
-public:
+void convertUserInformation(User*& user, string *const userInfo);
 
-};
+void getEquipments(Tent*& tents, Stove*& stoves, Lantern*& lanterns);
+
+void updateEquipments(Tent *const &tents, Stove *const &stoves, Lantern *const &lanterns);
+
+void getLoanRecord(string **&records, int &size);
+
+void updateLoanRecord(string *const*const records, int size);
+
+void performDisplayLoanAvailableItem(Tent *const &tents, Stove *const &stoves, Lantern *const &lanterns);
+
+void performBorrowEquipment(User *const &user, Tent *const &tents, Stove *const &stoves, Lantern *const &lanterns, string**& records, int &size);
+
+string **getUserCurrentBorrowRecord(User* const& user, string *const*const records, int &size, int &idSize);
+
+void performDisplayUserBorrowedItem(User *const &user, Tent *const &tents, Stove *const &stoves, Lantern *const &lanterns, string** &records, int &size);
+
+void performReturnItem(User* const& user, Tent* const& tents, Stove* const& stoves, Lantern* const& lanterns, string** &records, int &size);
 
 #endif
