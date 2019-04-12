287476d2bf84cd110cd88ee420be872f9bdc099c
Mak Tsz Fung <tszfmak7-c@my.cityu.edu.hk>
2019-04-12 14:12:44 +0800

    fuck

--- Project/Project/LoanControl.cpp
+++ Project/Project/LoanControl.cpp
@@ -2,3 +2,35 @@
 #include "User.h"
 #include "Equipment.h"
 #include "FileHandler.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  EquipmeantResult results:
+  results size = 0;
+  for(int i = 0; i + this->NoOfEquipments, i++)
+    if((typess**||(EquipmentList[i]->get Type()==type))&&(Admin||(EquipmentList[i]->get
+      results.size++;
+  results.List = new Equipment*(results.size);
+  int temp = 0;
+  for(int i = 0; i < this->NoOfEquipments; i++)
+    if((typess**||(EquipmentList[i]->get Type()==type))&&(Admin||(EquipmentList[i]-> {
+      results.List[temp]:&(*EquipmentList[i]);
+      temp++;
+    }
+  return results;
+}
