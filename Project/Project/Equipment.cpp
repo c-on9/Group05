f692225367097f3134580bcd89939e664e1a4d0d
Chris Lam <tungllui2-c@my.cityu.edu.hk>
2019-04-12 16:23:27 +0800

    formatting

--- Project/Project/Equipment.cpp
+++ Project/Project/Equipment.cpp
@@ -1,50 +1,41 @@
 #include "Equipment.h"
 
-string Equipment::getId() const
-{
+string Equipment::getId() const {
 	return id;
 }
 
-string Equipment::getItemId() const
-{
+string Equipment::getItemId() const {
 	return itemid;
 }
 
-string Equipment::getItemName() const
-{
+string Equipment::getItemName() const {
 	return itemname;
 }
 
-string Equipment::getBrand() const
-{
+string Equipment::getBrand() const {
 	return brand;
 }
 
-string Equipment::getItemType() const
-{
+string Equipment::getItemType() const {
 	return itemtype;
 }
 
-string Equipment::getDate() const
-{
+string Equipment::getDate() const {
 	return date;
 }
 
-string Equipment::getCondition() const
-{
+string Equipment::getCondition() const {
 	return condition;
 }
 
-string Equipment::getStatus() const
-{
+string Equipment::getStatus() const {
 	return status;
 }
 
-void Equipment::setCondition()
-{
+void Equipment::setCondition() {
 
 }
 
-void Equipment::setStatus()
-{
+void Equipment::setStatus() {
+    
 }
