8586a4eb278f4222589b73372e2729e2d68f938f
Ylferif <hclin8-c@ad.cityu.edu.hk>
2019-04-13 11:50:53 +0800

    gg

--- Project/Project/Main.cpp
+++ Project/Project/Main.cpp
@@ -1,1 +1,16 @@
+#include "Menu.h"
+#include <iostream>
+using namespace std;
 
+int main() {
+	bool quit;
+	int size;
+	int* arr = NULL;
+	do {
+		quit = executeMenu(selectMenu(), arr, size);
+	} while (!quit);
+	if (arr != NULL)
+		delete[] arr;
+	system("pause");
+	return 0;
+}
--- Project/Project/Menu.h
+++ Project/Project/Menu.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef MENU_H
+#define MENU_H
+
+// not done yet
+int userMenu()
+{
+	system("cls");
+	int choice;
+	do {
+		cout << "Select function: " << endl;
+		cout << "----------------" << endl;
+		cout << "1) Available equipments" << endl;
+		cout << "2) Loan record" << endl;
+		cout << "3) Borrow" << hint << endl;
+		cout << "4) Return" << endl;
+		cout << "9) Exit " << endl;
+		cout << endl;
+		cout << "Your choice: ";
+		cin >> choice;
+	} while (choice < 1 || choice > 6);
+	return choice;
+}
+
+#endif
