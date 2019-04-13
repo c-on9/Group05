3b8d000033093b117f5d227d44587be3a3972fb4
Chris Lam <tungllui2-c@my.cityu.edu.hk>
2019-04-13 10:07:24 +0800

    update Admin.h

--- Project/Project/Admin.h
+++ Project/Project/Admin.h
@@ -1,4 +1,27 @@
+#include "Equipment.h"
+#include "FileHandler.h"
 #ifndef ADMIN_H
 #define ADMIN_H
+#include <string>
+#include <fstream>
 
-class Admin
+using namespace std;
+
+class Admin {
+public:
+    void loanRecord(string , string, string, string, string, string);
+    void displayLoanRecord();
+    void displayItemsByCat();
+    void produceNewTxt();
+    
+private:
+    string userId;
+    string password;
+    string loanDate;
+    string itemId;
+    string itemName;
+    string borrowerName;
+    string returnDate;
+    string status;
+};
+#endif
